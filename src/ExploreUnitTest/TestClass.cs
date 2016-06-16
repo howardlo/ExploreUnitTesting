40c5ac78aac2f927eeab2f3c39d874f888e0b33b
Howard Lo <howardlo@outlook.com>
2016-06-16 09:52:15 -0500

    init

---
+++ src/ExploreUnitTest/TestClass.cs
@@ -0,0 +1,39 @@
+﻿using ExploreUnitTesting;
+using Microsoft.Owin.Testing;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExploreUnitTestingTest
+{
+    [TestClass]
+    public class TestClass
+    {
+        [TestMethod]
+        public async Task TestStartup()
+        {
+            using (var server = TestServer.Create<Startup>())
+            {
+                var response = await server.HttpClient.GetAsync("/nancy-test");
+                var body = await response.Content.ReadAsStringAsync();
+                Console.WriteLine("body: ");
+                Console.WriteLine(body);
+                Assert.IsTrue(body.Contains("nancy-test"));
+            }
+        }
+
+        //[TestMethod]
+        //public void TestMethodPassing()
+        //{
+        //    Assert.IsTrue(true);
+        //}
+
+        //[TestMethod]
+        //public void TestMethodFailing()
+        //{
+        //    Assert.IsTrue(false);
+        //}
+    }
+}
